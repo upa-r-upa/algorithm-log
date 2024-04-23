47f58defe1a214b0693ea4f5d97dd620e10685e4
upa-r-upa <me.uparupa@gmail.com>
2024-04-23 19:38:43 +0900

    [백준] 2750: 수 정렬하기 (병합 정렬)

--- 2750.py
+++ 2750.py
@@ -1,14 +1,31 @@
-def swap(array, idx_a, idx_b):
-	temp = array[idx_a]
-	array[idx_a] = array[idx_b]
-	array[idx_b] = temp
+def merge_sort_inner(arr1, arr2):
+	i = j = 0
+	result = []
+	
+	while i < len(arr1) and j < len(arr2):
+		if arr1[i] <= arr2[j]:
+			result.append(arr1[i])
+			i += 1
+		else:
+			result.append(arr2[j])
+			j += 1
+
+	result.extend(arr1[i:])
+	result.extend(arr2[j:])
+
+	return result
+
+def merge_sort(array):
+	if len(array) <= 1: 
+		return array
+
+	mid = len(array) // 2
+
+	left = merge_sort(array[:mid])
+	right = merge_sort(array[mid:])
+
+	return merge_sort_inner(left, right)
 
 n = int(input())
-result = [int(input()) for i in range(n)]
-
-for i in range(n):
-	for idx in range(n-i-1):
-		if result[idx] > result[idx + 1]:
-			swap(result, idx, idx + 1)
-	
-print(*result, sep="\n")
+arr = [int(input()) for i in range(n)]
+print(*merge_sort(arr), sep="\n")
