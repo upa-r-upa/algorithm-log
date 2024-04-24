cc13efb9768c2129dfeaec42c9c638b6f80297bc
upa-r-upa <me.uparupa@gmail.com>
2024-04-24 12:07:28 +0900

    [백준] 2750: 수 정렬하기 (선택 정렬)

--- 2750.py
+++ 2750.py
@@ -1,31 +1,18 @@
-def merge_sort_inner(arr1, arr2):
-	i = j = 0
-	result = []
-	
-	while i < len(arr1) and j < len(arr2):
-		if arr1[i] <= arr2[j]:
-			result.append(arr1[i])
-			i += 1
-		else:
-			result.append(arr2[j])
-			j += 1
+def swap(arr, idx_a, idx_b):
+    temp = arr[idx_a]
 
-	result.extend(arr1[i:])
-	result.extend(arr2[j:])
-
-	return result
-
-def merge_sort(array):
-	if len(array) <= 1: 
-		return array
-
-	mid = len(array) // 2
-
-	left = merge_sort(array[:mid])
-	right = merge_sort(array[mid:])
-
-	return merge_sort_inner(left, right)
+    arr[idx_a] = arr[idx_b]
+    arr[idx_b] = temp
 
 n = int(input())
-arr = [int(input()) for i in range(n)]
-print(*merge_sort(arr), sep="\n")
+result = [int(input()) for i in range(n)]
+
+for i in range(n):
+    min_i = i
+    for idx in range(i+1,n):
+        if result[min_i] > result[idx]:
+            min_i = idx
+    if min_i != i:
+        swap(result, i, min_i)
+
+print(*result, sep="\n")
