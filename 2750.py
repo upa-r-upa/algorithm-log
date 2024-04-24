e54912ce5092dfbe0ae0ebec9aef22b97fd905f9
upa-r-upa <me.uparupa@gmail.com>
2024-04-24 20:06:20 +0900

    [백준] 2750: 수 정렬하기 (삽입 정렬)

--- 2750.py
+++ 2750.py
@@ -1,18 +1,14 @@
-def swap(arr, idx_a, idx_b):
-    temp = arr[idx_a]
-
-    arr[idx_a] = arr[idx_b]
-    arr[idx_b] = temp
-
 n = int(input())
 result = [int(input()) for i in range(n)]
 
-for i in range(n):
-    min_i = i
-    for idx in range(i+1,n):
-        if result[min_i] > result[idx]:
-            min_i = idx
-    if min_i != i:
-        swap(result, i, min_i)
+for i in range(1, n):
+	value = result[i]
+	j = i
+
+	while j > 0 and result[j-1] > value:
+		result[j] = result[j-1]
+		j -= 1
+	
+	result[j] = value
 
 print(*result, sep="\n")
