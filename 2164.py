272856175802e128cfba06bdca11acbe538206f0
upa-r-upa <me.uparupa@gmail.com>
2024-05-13 22:07:48 +0900

    [백준] 2164: 카드 2 (deque 사용)

--- 2164.py
+++ 2164.py
@@ -1,12 +1,10 @@
+from collections import deque
+
 n = int(input())
-queue = [i for i in range(1,n+1)]
-idx = 0
+queue = deque(range(1, n + 1))
 
-while len(queue) - 1 > idx + 1:
-	idx += 1
+while len(queue) > 1:
+	queue.popleft()
+	queue.append(queue.popleft())
 
-	if len(queue) - 1 > idx + 2:
-		queue.append(queue[idx])
-		idx += 1
-
-print(queue[len(queue)-1])
+print(queue[0])
