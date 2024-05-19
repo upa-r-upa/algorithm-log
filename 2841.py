a9ab6aa86e88dcc359825c4fb167f41c6fb71589
upa-r-upa <me.uparupa@gmail.com>
2024-05-20 03:18:17 +0900

    [백준] 2841: 외계인의 기타 연주 (개선 버전)

--- 2841.py
+++ 2841.py
@@ -1,38 +1,23 @@
 import sys
 
-n, p_max = list(map(int, input().split(" ")))
+n, p_max = list(map(int, input().split()))
 guitars = [[] for i in range(6)]
 count = 0
 
 for i in range(n):
-	line, p = list(map(int, sys.stdin.readline().rstrip().split(" ")))
+	line, p = list(map(int, sys.stdin.readline().rstrip().split()))
 	fingers = guitars[line-1]
 	
-	if len(fingers) == 0 or fingers[len(fingers)-1] < p:
+	if len(fingers) == 0 or fingers[-1] < p:
 		count += 1
 		fingers.append(p)
-	elif fingers[len(fingers)-1] > p:
-			if fingers[0] == p:
-				count += len(fingers) - 1
-				fingers = [p]
-			elif fingers[0] > p:
-				count += len(fingers) + 1
-				fingers = [p]
-
-			else:
-				for j in range(len(fingers)):
-					if fingers[j] > p:
-						count += len(fingers) - j + 1
-						fingers = fingers[:j]
-						fingers.append(p)
-						
-						break
-					elif fingers[j] == p:
-						count += len(fingers) - j - 1
-						fingers = fingers[:j+1]
-						
-						break
-	
-	guitars[line-1] = fingers
+	elif fingers[-1] > p:
+		while len(fingers) > 0 and fingers[-1] > p:
+			fingers.pop()
+			count += 1
+		
+		if len(fingers) == 0 or fingers[-1] != p:
+			fingers.append(p)
+			count += 1
 	
 print(count)
