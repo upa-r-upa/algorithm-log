02a82d342c139daad804ec0860a4123228146a2c
upa-r-upa <me.uparupa@gmail.com>
2024-05-27 19:49:27 +0900

    [백준] 2745: 진법 변환

---
+++ 2745.py
@@ -0,0 +1,11 @@
+b,n = input().split()
+print(int(b, int(n)))
+
+# b,n = input().split()
+# result=0
+
+# for i in range(len(b)):
+# 	digit = int(b[len(b)-1-i], int(n))
+# 	result += digit * (int(n) ** i)
+
+# print(result)
