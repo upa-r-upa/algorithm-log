b838c7d54b0bbc2f3a2d97343080de6bb953b247
upa-r-upa <me.uparupa@gmail.com>
2024-06-20 17:50:58 +0900

    [백준] 후위 표기식 (개선)

--- 1918.py
+++ 1918.py
@@ -1,35 +1,26 @@
 import sys
-
-formula=sys.stdin.readline().rstrip()
-operators=[]
+text=sys.stdin.readline().rstrip()
+stack=[]
 result=[]
+priority={"+": 1, "-": 1, "*": 2, "/": 2, "(": 0}
 
-for i in range(len(formula)):
-    if formula[i] == "(":
-        operators.append([])
-    elif formula[i] == ")":
-        while operators[-1]:
-            result.append(operators[-1].pop())
-
-        operators.pop()
-    elif formula[i].isalpha():
-        result.append(formula[i])
-    else:
-        if len(operators) == 0:
-            operators.append([])
-
-        if formula[i] == "+" or formula[i] == "-":
-            while operators[-1]:
-                result.append(operators[-1].pop())
-
-            operators[-1].append(formula[i])
-        else:
-            if len(operators[-1]) >= 2 or (operators[-1] and (operators[-1][-1] == "*" or operators[-1][-1] == "/")):
-                result.append(operators[-1].pop())
-
-            operators[-1].append(formula[i])
-
-if operators:
-    result.extend(reversed(operators[-1]))
-
+for i in range(len(text)):
+	if text[i].isalpha():
+		result.append(text[i])
+	elif text[i] == "(":
+		stack.append("(")
+	elif text[i] == ")":
+		while stack and stack[-1] != "(":
+			result.append(stack.pop())
+			
+		stack.pop()
+	else:
+		while stack and priority[stack[-1]] >= priority[text[i]]:
+			result.append(stack.pop())
+		
+		stack.append(text[i])
+		
+while stack:
+	result.append(stack.pop())
+		
 print(*result,sep="")
