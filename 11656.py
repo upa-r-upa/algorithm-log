0abbb2481a373a5e15fdd5b3b2103ffff157bc11
upa-r-upa <me.uparupa@gmail.com>
2024-06-25 16:11:28 +0900

    [백준] 접미사 배열 - 선택 배열

--- 11656.py
+++ 11656.py
@@ -1,9 +1,21 @@
 import sys
 
+def selection_sort(arr):
+	for i in range(len(arr)):
+		min_idx=i
+		
+		for j in range(i,len(arr)):
+			if arr[min_idx] > arr[j]:
+				min_idx=j
+		
+		arr[i],arr[min_idx]=arr[min_idx],arr[i]
+		
+	return arr
+
 s=sys.stdin.readline().rstrip()
 result=[]
 
 for i in range(len(s)):
 	result.append(s[i:])
 	
-print(*sorted(result),sep="\n")
+print(*selection_sort(result),sep="\n")
