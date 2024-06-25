55261c9f1ba7e2bd7c533699d56a37d20b83d078
upa-r-upa <me.uparupa@gmail.com>
2024-06-25 18:52:20 +0900

    [백준] 접미사 배열 - 삽입 정렬

--- 11656.py
+++ 11656.py
@@ -1,40 +1,23 @@
 import sys
 
-def merge_sort(arr):
-	if len(arr) == 1:
-		return arr
+def insertion_sort(arr):
+	for i in range(1,len(arr)):
+		curr = arr[i]
+		j = i-1
 		
-	pivot=len(arr)//2
+		while j >= 0 and arr[j] > curr:
+			arr[j+1] = arr[j]
+			j -= 1
+		
+		arr[j+1] = curr
 	
-	left=merge_sort(arr[:pivot])
-	right=merge_sort(arr[pivot:])
-	
-	return merge_arr(left,right)
-
-def merge_arr(left,right):
-	result = []
-	
-	i=j=0
-	
-	while len(left) > i and len(right) > j:
-		if left[i] <= right[j]:
-			result.append(left[i])
-			i+=1
-		else:
-			result.append(right[j])
-			j+=1
-	while i < len(left):
-		result.append(left[i])
-		i+=1
-	while j < len(right):
-		result.append(right[j])
-		j+=1
-	return result
-    
+	return arr
+			
+			
 s=sys.stdin.readline().rstrip()
 result=[]
 
 for i in range(len(s)):
 	result.append(s[i:])
 	
-print(*selection_sort(result),sep="\n")
+print(*insertion_sort(result),sep="\n")
