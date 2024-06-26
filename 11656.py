440fb4f3993dd0fb100ac9421d0aa15a3a1e7d4c
upa-r-upa <me.uparupa@gmail.com>
2024-06-26 16:56:43 +0900

    [백준] 접미사 배열 - quick sort

--- 11656.py
+++ 11656.py
@@ -1,23 +1,38 @@
 import sys
 
-def insertion_sort(arr):
-	for i in range(1,len(arr)):
-		curr = arr[i]
-		j = i-1
-		
-		while j >= 0 and arr[j] > curr:
-			arr[j+1] = arr[j]
-			j -= 1
-		
-		arr[j+1] = curr
-	
-	return arr
-			
-			
+def quick_sort(arr):
+    def partition(arr, start, end) -> int:
+        pivot = arr[(start+end)//2]
+        
+        while start <= end:
+            while arr[start] < pivot:
+                start += 1
+            while arr[end] > pivot:
+                end -= 1
+            if start<=end:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+        
+        return start
+        
+    def sort(arr, start, end):
+        if end <= start:
+            return arr
+            
+        pivot = partition(arr, start, end)
+        
+        sort(arr, start, pivot-1)
+        sort(arr, pivot, end)
+        
+        return arr
+    
+    return sort(arr, 0, len(arr)-1)
+
 s=sys.stdin.readline().rstrip()
 result=[]
 
 for i in range(len(s)):
 	result.append(s[i:])
 	
-print(*insertion_sort(result),sep="\n")
+print(*quick_sort(result),sep="\n")
