bc4aa225cb65333583685fba4215d3308c84c860
upa-r-upa <me.uparupa@gmail.com>
2024-06-28 23:29:25 +0900

    [백준] 최소 힙 (heapq 사용)

---
+++ 11279.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+n=int(sys.stdin.readline().rstrip())
+heap = []
+
+for _ in range(n):
+	x=int(sys.stdin.readline().rstrip())
+	
+	if x==0:
+		if heap:
+			print(-heapq.heappop(heap))
+		else:
+			print(0)
+	else:
+		heapq.heappush(heap, -x)
