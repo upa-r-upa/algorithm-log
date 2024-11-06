ab6ad76b7a3b39342e1c26bfaa2faf6ad74af41a
upa-r-upa <me.uparupa@gmail.com>
2024-11-06 19:40:19 +0900

    [백준] 요세푸스 (리뉴얼)

--- 1158.py
+++ 1158.py
@@ -1,11 +1,14 @@
-n, k = map(int, input().split())
-idx = 0
-people = [i for i in range(1, n+1)]
+n,k = map(int, input().split())
+people = [i+1 for i in range(n)]
 result = []
+curr_index = 0
 
 while people:
-	next_idx = (idx + k - 1) % len(people)
-	result.append(people.pop(next_idx))
-	idx = next_idx
+	index = (curr_index + k - 1) % len(people)
+	result.append(people.pop(index))
 
-print("<" + ", ".join(map(str, result)) + ">")
+	curr_index = index
+
+print("<", end="")
+print(*result, sep=", ", end="")
+print(">")
