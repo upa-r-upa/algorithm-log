5b9ff9adeb5795905f96dc36b86373f0c86907f6
upa-r-upa <me.uparupa@gmail.com>
2024-11-07 19:32:25 +0900

    [프로그래머스] 기능개발 (다른 방식)

--- 42586.py
+++ 42586.py
@@ -1,22 +1,22 @@
-def check(progresses, speeds, day):
-    return progresses[0] + day * speeds[0] >= 100
+import math
 
 def solution(progresses, speeds):
-    day = 1
-    deploys = [0]
+    work_days = []
+    result = []
     
-    while len(progresses) > 0:
-        if check(progresses, speeds, day):
-            progresses.pop(0)
-            speeds.pop(0)
-            deploys[-1] += 1
-            
-            if len(progresses) != 0 and check(progresses, speeds, day) is False:
-                deploys.append(0)
-                day = 1
-        else:
-            day = -(((100 - progresses[0]) / speeds[0]) // -1)
+    for index in range(len(progresses)):
+        days =  math.ceil((100 - progresses[index]) / speeds[index])
+        work_days.append(days)
     
+    i = 0
+    while i < len(work_days):
+        j = i
+        
+        while j+1 < len(work_days) and work_days[i] >= work_days[j+1]:
+            j += 1
+        
+        result.append(j-i+1)
+        
+        i = j+1
     
-    return deploys
-            
+    return result
