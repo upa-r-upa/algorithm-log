fa31f46f0bb9589a2903cecf93b75dc64a6b20b4
upa-r-upa <me.uparupa@gmail.com>
2024-11-22 19:50:59 +0900

    [프로그래머스] 타겟 넘버

--- 43165.py
+++ 43165.py
@@ -1,4 +1,5 @@
-def solution(numbers, target):
+# DFS
+def solution_dfs(numbers, target):
     count = 0
     
     def dfs(idx, total):
@@ -13,5 +14,31 @@ def solution(numbers, target):
         dfs(idx + 1, total - numbers[idx])
         
     dfs(0,0)
+    
+    return count
+
+# BFS
+from collections import deque
+
+def solution_bfs(numbers, target):
+    count = 0
+    
+    def dfs():
+        queue = deque([(0,0)])
+        nonlocal count
+        
+        while queue:
+            idx, total = queue.popleft()
+            
+            if idx == len(numbers):
+                if total == target:
+                    count += 1
+                    
+                continue
+        
+            queue.append((idx + 1, total + numbers[idx]))
+            queue.append((idx + 1, total - numbers[idx]))
+        
+    dfs()
     
     return count
